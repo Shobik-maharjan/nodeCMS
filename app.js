ff444ea78b3fa08091b5c78fe77e5f49b0d7ee89
Shobik maharjan <shobitmah32@gmail.com>
2023-11-05 17:32:14 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { blogs } = require("./model/index.js");
+const app = express();
+// telling nodejs to require and use .env
+require("dotenv").config();
+
+require("./model/index.js");
+// say nodejs that we are using ejs, set everything
+app.set("view engine", "ejs");
+
+// telling nodejs to accept the incoming data(parsing data)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("allBlogs.ejs");
+});
+
+app.get("/addBlog", (req, res) => {
+  res.render("addBlog");
+});
+
+// api for handling formdata
+app.post("/addBlog", async (req, res) => {
+  // const title = req.body.title
+  // const subTitle = req.body.subTitle
+
+  // ALTERNATIVE
+
+  const { title, subTitle, description } = req.body;
+  await blogs.create({
+    title: title,
+    subTitle: subTitle,
+    description: description,
+  });
+  res.send("BLog created successfully");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Node js project has started at port " + PORT);
+});
